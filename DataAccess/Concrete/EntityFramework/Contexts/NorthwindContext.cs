9e2bd601f4483e5e3b5f69148a93b420af1ba36b
Abdulkadir KOÇAK <kadir.kocak.50@hotmail.com>
2019-11-27 23:41:35 +0100

    İnstalled AutoFac. Edit SQL Connection string. AutoFac, Program.cs'de yapılandırılmıştır.

--- DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)/mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
         }
         public DbSet<Product> Products { get; set; }
     }
