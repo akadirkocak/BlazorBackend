697df2a4400257bfb5e1516c1568ac0ed19f08fc
Abdulkadir KOÇAK <kadir.kocak.50@hotmail.com>
2019-12-03 12:33:17 +0100

    Added IDto, USerForLogin and USerForRegister in Entities.

---
+++ Entities/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Entities.Dtos
+{
+    class UserForRegisterDto:IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirtName { get; set; }
+        public string LastName { get; set; }
+    }
+}
