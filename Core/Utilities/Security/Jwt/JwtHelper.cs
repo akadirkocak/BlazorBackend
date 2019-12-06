0c633baee9ddeff11ce6e1a1f7a5b1e6c8deb968
Abdulkadir KOÇAK <kadir.kocak.50@hotmail.com>
2019-12-06 12:25:06 +0100

    Added JwtHelper and TokenOptions.

---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concreate;
+using Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper:ITokenHelper
+    {
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get;}
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
