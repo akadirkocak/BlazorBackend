c11178c784eb2a2dc340ff862fde85432d17434c
Abdulkadir KOÇAK <kadir.kocak.50@hotmail.com>
2019-12-06 12:36:27 +0100

    Added SecurityKeyHelper. Edit JwtHelper.

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using Core.Entities.Concreate;
+using Core.Utilities.Security.Encyption;
 using Entities.Concrete;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Core.Utilities.Security.Jwt
 {
     public class JwtHelper:ITokenHelper
     {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
         public JwtHelper(IConfiguration configuration)
         {
             Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
         }
 
-        public IConfiguration Configuration { get;}
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+
         }
     }
 }
